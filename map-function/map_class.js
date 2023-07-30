5a8b3c5cfc4a810418da59c28c043cc2050ff254
johnenes <johneneojo1@gmail.com>
2023-07-30 03:01:31 +0000

    map function constain setting map object in three ways also retrieved by get or iterations

--- map-function/map_class.js
+++ map-function/map_class.js
@@ -1,4 +1,45 @@
-let myMap = Object.create(null);
-myMap.name = "john";
-let firstname = myMap.name;
-console.log(firstname);
+//working with set in javascript
+
+let mySet = Object.create(null);
+mySet.id = 1;
+if(mySet.id){
+    console.log('id exit')
+}
+
+//map is used for retrieving a vlaue of key-pair
+let myMap = Object.create(null); //new object
+myMap.firstName = "john";
+myMap.lastName = "Nelson"
+let first_name = myMap.firstName;
+let last_name = myMap.lastName;
+console.log(first_name, last_name);
+
+
+let setMap = new Map();//creating map object
+setMap.set('name','john');
+setMap.set('age' ,29)
+console.log(setMap.get('name'));
+console.log(setMap.get('age'))
+
+
+
+//iterating through map
+
+let menu = new Map([
+    ['home', 'Home'],
+    ['about','About'],
+    ['contact','Contact'],
+    ['skill','skills']
+])
+ 
+//retrieving only the key from then map
+for(let key of menu.keys()){
+    console.log(key);
+}
+
+//retrieving only the values of the map
+for (let value of menu.values()){
+    console.log(value);
+}
+
+
