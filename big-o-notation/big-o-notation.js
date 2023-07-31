adafc570eb0ad267260511c7ff6ba086b1298728
johnenes <johneneojo1@gmail.com>
2023-07-31 02:24:57 +0000

    Big O Algorthim

---
+++ big-o-notation/big-o-notation.js
@@ -0,0 +1,31 @@
+//BIG O NOTATION IS 
+
+
+//  Big o Notation is express interm of input 
+//  it focus on big picture without me caught up in the  minute details
+
+// The sum of natural number 
+ const summation =(n)=>{
+    let sum = 0;
+    for(let i = 1; i <= n; i++){
+        sum += i;
+    }
+    return sum;
+}
+console.log(summation(4));
+
+
+// multiple algorithm exit for the same problem  there is no one right solution.
+//different algorithm work well under different constraint.
+
+// Always understand the problem before determining wish one is better.
+
+// the same algorithm with the same programming language can be implemented in different way
+
+// when writing programs at work don't lose sight of  the picture. Rether than write a clever code, write 
+//code that is  simple to read and maintain 
+
+
+
+
+
